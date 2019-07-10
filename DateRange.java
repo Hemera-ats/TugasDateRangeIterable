a379b8e0d159e1affb4a936a495d457eead95182
Hemera-ats <51821803+Hemera-ats@users.noreply.github.com>
2019-07-11 00:22:21 +0700

    Update DateRange.java

--- DateRange.java
+++ DateRange.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package testdaterange;
 import java.time.LocalDate;
     
@@ -19,7 +14,6 @@ public class DateRange implements Iterable<LocalDate> {
         this.sekarang = awal;
     }
 
-    @Override
     public Iterator<LocalDate> iterator() {
         return new DateRangeIterator(this.awal,this.akhir);
     }
